d0297a7b3eb550b4fd81d8595b5f3c7965427d51
kowalsky2608 <kowalsky2608@wp.pl>
2024-03-18 12:37:32 +0100

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000
+
+const app = express()
+app.set('view engine','hbs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.text())
+// app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on ${port}`)
+})
