109ba989402481b5c6c21ac836168b8c059f0284
kowalsky2608 <kowalsky2608@wp.pl>
2024-03-18 13:25:45 +0100

    fix port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 app.set('view engine','hbs')
