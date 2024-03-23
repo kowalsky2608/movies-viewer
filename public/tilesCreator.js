5f7502f3a2d161bba51e431af88bf4019eba36d7
kowalsky2608 <kowalsky2608@wp.pl>
2024-03-23 23:56:19 +0100

    add admin panel

---
+++ public/tilesCreator.js
@@ -0,0 +1,5 @@
+function tileCreator(){
+    const tileContainer = document.createElement('div')
+    const movieTitle = document.getElementById('title').value
+    const moviePoster = document.getElementById('poster').value
+}
